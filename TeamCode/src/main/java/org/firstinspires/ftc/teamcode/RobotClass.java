7416a3302366b1dd9319105a76da8241550a094a
thedestroyerofworlds <themathnight@gmail.com>
2025-03-06 21:49:38 -0700

    fixed sushies TODOS and prepared for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClass.java
@@ -6,8 +6,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
 public class RobotClass {
         public DcMotorEx frontLeft, frontRight, backRight, backLeft;
 
