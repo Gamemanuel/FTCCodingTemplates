573c1150d4a767c2e39c5aea0272aea9142fb3f5
Dragons <10523dragons@gmail.com>
2025-09-04 14:15:16 -0700

    Fixed 6 wheel auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SixWheelAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SixWheelAutoTest.java
@@ -4,25 +4,26 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous
-public class SixWheelAutoTest extends LinearOpMode { //TODO: no matter the configuration, the motors go brrr clackclackclackclackclack
+public class SixWheelAutoTest extends LinearOpMode {
     RobotClass robot;
 
     @Override
     public void runOpMode() {
         robot = new RobotClass(hardwareMap);
         waitForStart();
-        while (!isStopRequested()) {
-            driveBasic(0.75, 0.75, 1);
-            driveBasic(-0.75, -0.75, 1);
-            driveBasic(0.75, -0.75, 1);
-            driveBasic(-0.75, 0.75, 1);
-        }
+            driveBasic(0.5, 0.5, 1000);
+        sleep(1000);
+            driveBasic(-0.5, -0.5, 1000);
+        sleep(1000);
+            driveBasic(-0.75, 0.75, 1000);
+        sleep(1000);
+            driveBasic(0.75, -0.75, 1000);
     }
     public void driveBasic(double left,double right,long time) { //left: left side, right: right side, time: for how long
         robot.frontLeft.setPower(left);
         robot.backLeft.setPower(left);
         robot.frontRight.setPower(right);
-        robot.backRight.setPower(-right);
+        robot.backRight.setPower(right);
         //set all motors
         sleep(time); //wait for however long
         robot.frontLeft.setPower(0);
